63a524b8c63a7dd02126da5e222ddae5780ab010
Jagadishbommareddy <jagadish6060@gmail.com>
2019-04-22 14:58:16 +0530

    changes in models, views and templates

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from empolyee import views
+from employee import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emp', views.emp),
--- empolyee/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from empolyee.models import *
-
-class EmployeeForm(forms.ModelForm):
-    class Meta:
-        model = Employee
-        fields = "__all__"
